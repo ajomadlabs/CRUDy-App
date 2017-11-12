8cbe607c68aa417ff71b7b2d0b25fe42ef124e6f
ajomadlabs <ajojohn555@gmail.com>
2017-11-13 02:22:28 +0530

    Backend: Delete Route Done
    Created a delete route
    Data is taken on the basis of id
    
    Fixes #6 - Delete Route Made

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -69,4 +69,35 @@ module.exports = function(app, db) {
 
     });
 
+    // Delete Route
+    // Use remove function to delete
+    // Take id from URL
+
+    app.delete('/notes/:id', (req, res) => {
+
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+
+        db.collection('notes').remove(details, (err, item) => {
+
+            if (err) {
+
+                res.send({
+
+                    'error': 'An error has occurred'
+
+                });
+
+            }
+
+            else {
+
+                res.send('Note' + id + 'deleted');
+        
+            }
+
+        });
+
+    });
+
 };
